75e64c1f3f687a8e9c8f22af2dc87ea370ce25e3
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-11-05 15:58:13 -0500

    seed js updated

---
+++ seeds/seed.js
@@ -0,0 +1,18 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const seedUsers = require('./userData');
+const seedPosts = require('./postData');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+
+    await seedUsers();
+
+    await seedPosts();
+
+    process.exit(0);
+}
+
+seedAll();  
+
