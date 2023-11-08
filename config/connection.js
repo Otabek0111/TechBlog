5883bd68eca3fa2c1878bd0d28134934d22f815d
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-11-08 15:01:50 -0500

    connection js updated

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
 // If the server contains the JAWSDB_URL environmental variable, it connects to the JawsDB database.
 if (process.env.JAWSDB_URL) {
-    sequelize = new sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     // Otherwise, it connects to the local MySQL database.
-    sequelize = new sequelize(
+    sequelize = new Sequelize(
         process.env.DB_NAME, 
         process.env.DB_USER, 
         process.env.DB_PW, 
