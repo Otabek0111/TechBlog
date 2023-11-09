22aa3da3e77cb5009be1e7f8614dc94faa3c7528
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-11-09 13:06:19 -0500

    helper updated'

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,12 @@
-module.exports ={
-    format_date: date => { 
-        // Format date as MM/DD/YYYY
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-    }
-}
+module.exports = {
+  format_date: (date) => {
+    // Format date and time information to MM/DD/YYYY HH:mm
+    const d = new Date(date);
+    const formattedDate = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+    const hours = d.getHours().toString().padStart(2, '0');
+    const minutes = d.getMinutes().toString().padStart(2, '0');
+    const formattedTime = `${hours}:${minutes}`;
+    
+    return `${formattedDate} at ${formattedTime}`;
+  },
+};
